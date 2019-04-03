bdda9734e2b6f50f5fe33fa336202fc00b77ad68
minzhang <a991391009@gmail.com>
2019-04-03 17:33:12 +0800

    static file basic

--- backend/backend/local.py
+++ backend/backend/local.py
@@ -11,8 +11,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 from backend.settings import *
-
 import os
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,8 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 # INSTALLED_APPS += (
 #     'debug_toolbar', # and other apps for local development
 # )
